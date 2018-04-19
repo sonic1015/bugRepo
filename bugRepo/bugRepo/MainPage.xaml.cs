223fc6f0e35b5d2abcd93397e3a5339d73de0827
sonic1015 <evin@asi.bike>
2018-04-19 16:29:32 -0400

    - Created TriggerAction Property Bug

--- bugRepo/bugRepo/MainPage.xaml.cs
+++ bugRepo/bugRepo/MainPage.xaml.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace bugRepo
 {
-	public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
